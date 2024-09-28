9a32c05e0cb96aa9d239dacf3dae14c79b2b7f4c
vinsontandraa <vtandra7@gmail.com>
2024-09-28 17:24:17 +0700

    mitra fix

--- routes/mitra.js
+++ routes/mitra.js
@@ -15,14 +15,16 @@ const storage = multer.diskStorage({
   const upload = multer({ storage });
   
   // Route to handle POST request for Mitra creation with image upload
-  router.post('/mitras', upload.array('images', 10), async (req, res) => {
+  app.post('/mitras', upload.array('images', 10), async (req, res) => {
+    console.log('Request body:', req.body);
+    console.log('Uploaded files:', req.files);
     try {
       const imageFiles = req.files.map(file => file.filename);
       const mitra = new Mitra({ ...req.body, images: imageFiles });
       await mitra.save();
       res.status(201).json(mitra);
     } catch (err) {
-      console.error('Error creating Mitra:', err); // Log the actual error
+      console.error('Error creating Mitra:', err);
       res.status(500).json({ error: 'Server error while creating Mitra' });
     }
   });
