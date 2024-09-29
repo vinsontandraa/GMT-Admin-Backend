518aba836a9faba2e8a3ef9417fae025e4a255e1
vinsontandraa <vtandra7@gmail.com>
2024-09-29 17:31:22 +0700

    change to old auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,12 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
-const authMiddleware = async (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1]; // Splitting to get token after 'Bearer'
-    
+module.exports = (req, res, next) => {
+    const token = req.headers['authorization'];
     if (!token) return res.status(403).json({ error: 'No token provided' });
 
-
-    try {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) return res.status(403).json({ error: 'Failed to authenticate token' });
-            req.user = decoded; // Attach the decoded user to req.user
-            console.log("Decoded token:", decoded); // Log the decoded token
-            next();
-        });
-    } catch (err) {
-        res.status(400).json({ message: 'Invalid token.' });
-    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(403).json({ error: 'Failed to authenticate token' });
+        req.user = decoded;
+        next();
+    });
 };
-
-module.exports = authMiddleware;
