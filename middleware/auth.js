31a59788c7d55233903cf6fd2eea03c3e0fce654
vinsontandraa <vtandra7@gmail.com>
2024-09-29 17:28:45 +0700

    change auth method

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,18 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
+    const token = req.headers['authorization']?.split(' ')[1]; // Splitting to get token after 'Bearer'
     
-    if (!token) {
-        return res.status(401).json({ message: 'Access denied. No token provided.' });
-    }
+    if (!token) return res.status(403).json({ error: 'No token provided' });
+
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Ensure you have your secret key
-        req.user = await User.findById(decoded.id).select('-password'); // Get user details without the password
-        next();
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) return res.status(403).json({ error: 'Failed to authenticate token' });
+            req.user = decoded; // Attach the decoded user to req.user
+            console.log("Decoded token:", decoded); // Log the decoded token
+            next();
+        });
     } catch (err) {
         res.status(400).json({ message: 'Invalid token.' });
     }
