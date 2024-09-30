bd438e5a457dd892edd32463506970022b927539
vinsontandraa <vtandra7@gmail.com>
2024-10-01 03:05:13 +0700

    change user role

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'user', 'manager','supervisor'], default: 'user' }
+    role: { type: String, enum: ['admin1', 'admin2', 'admin3','admin5','admin6'], default: 'user' }
 });
 
 module.exports = mongoose.model('User', userSchema);
